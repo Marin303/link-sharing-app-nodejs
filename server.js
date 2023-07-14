7954c1672d8f6c35f3717cb8f637a4ded984ac38
Marin <marin.muktic3@gmail.com>
2023-07-14 15:30:32 +0200

    postman- response

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ require("dotenv").config();
 const app = express();
 const port = 5000;
 
+app.use(express.json())
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -11,6 +13,11 @@ app.get("/", (req, res) => {
 app.get("/preview", (req, res) => {
   res.send("Hello preview");
 });
+
+app.post('/preview', (req, res)=>{
+    console.log(req.body);
+    res.send(req.body)
+})
 
 const password = process.env.URI_PASSWORD;
 const uri = `mongodb+srv://Marin03:${password}@cluster0.ujwxme5.mongodb.net/Node-API?retryWrites=true&w=majority`;
