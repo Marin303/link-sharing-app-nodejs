269f2a7ee6f99d63e1522a34b3700392e4b41574
Marin <marin.muktic3@gmail.com>
2023-07-30 12:58:17 +0200

    disabling dotenv for production

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const multer = require("multer");
 const { v4: uuidv4 } = require("uuid");
-require("dotenv").config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 const cors = require("cors");
 const MongoClient = require("mongodb").MongoClient;
 
