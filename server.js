77ccd4a941e0f8f58243fea85812126ab064d642
Marin <marin.muktic3@gmail.com>
2023-07-30 14:21:13 +0200

    helmet for prevent content security policy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const multer = require("multer");
 const { v4: uuidv4 } = require("uuid");
+const helmet = require('helmet');
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
@@ -10,6 +11,13 @@ const MongoClient = require("mongodb").MongoClient;
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    imgSrc: ["'self'", 'data:'],
+  },
+}));
 
 app.use("/uploads", express.static("uploads"));
 app.use(express.json());
